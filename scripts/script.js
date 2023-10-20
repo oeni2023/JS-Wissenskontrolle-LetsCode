5e1d45a7622e81c213b54b9eb1270cf9ac85d435
oeni2023 <141681986+oeni2023@users.noreply.github.com>
2023-10-20 20:34:51 +0200

    finalised.

--- scripts/script.js
+++ scripts/script.js
@@ -12,30 +12,30 @@ function validateInput () {
     let text;
     if (userOption === "number") {
         if (isNaN(userInput) || userInput === undefined) {
-            text = `" ${userInput} " is NOT a Number!`;
+            text = `" ${userInput} " ist <strong>KEIN</strong> Nummer!`;
         } else {
-            text = `" ${userInput} " IS a number!`;
+            text = `" ${userInput} " <strong>IST</strong> eine Nummer!`;
         }
 
     } else if (userOption === "filled") {
-        if (userInput === undefined) {
-            text = `" ${userInput} " is NOT a ausgefüllt!`;
+        if (userInput === undefined || userInput === null || userInput === " " || userInput === "") {
+            text = `" ${userInput} " ist '<strong>NICHT</strong>' ausgefüllt!`;
         } else {
-            text = `" ${userInput} " IS a ausgefüllt!`;
+            text = `" ${userInput} " <strong>IST</strong> ausgefüllt!`;
         }
 
     } else if (userOption === "von10to20Zeichen") {
         if (userInput.length < 10 || userInput.length > 20) {
-            text = `" ${userInput} " is <strong>NOT</strong> in the range 10 - 20`;
+            text = `" ${userInput} " hat <strong>NICHT</strong> zwischen 10 and 20 Zeichen`;
         } else {
-            text = `" ${userInput} " <strong>IS</strong> in the range 10 - 20`;
+            text = `" ${userInput} " <strong>HAT</strong> zwischen 10 - 20 Zeichen`;
         }
 
     } else if (userOption === "containXand3and$") {
         if (userInput.includes("X") || userInput.includes("3") || userInput.includes("$")) {
-            text = `" ${userInput} " is <strong>BINGO</strong>`;
+            text = `" ${userInput} " <strong>ENTHÄLT</strong> ein "X", eine "3" oder ein "$"`;
         } else {
-            text = `<strong>BONGO</strong>`;
+            text = `" ${userInput} " enthält <strong>KEIN</strong> "X", eine "3" oder ein "$"`;
         }
     }
 
@@ -47,80 +47,17 @@ function validateInput () {
     // Log output-message;
     let logEntry = document.createElement('div');
     logEntry.className = "log-entry";
-    // logEntry.innerHTML = "test";
 
     const time = new Date();
+    const dateStr = time.toLocaleDateString();
     const timeStr = time.toLocaleTimeString();
 
-    let outPutString =`[${timeStr}] -- ${text}`;
+
+    let outPutString =`[${dateStr} - ${timeStr}] -- ${text}`;
     let outputSection = document.getElementById("l-text");
-    outputSection.append(outPutString);
+    // outputSection.append(outPutString);
+    outputSection.insertAdjacentHTML('beforeend', outPutString);
     linebreak = document.createElement("br");
     outputSection.append(linebreak);
 }
 
-
-//
-// /** Get input*/
-// function getInput() {
-//     let inputValue = document.getElementById("i-text").value;
-//     let inputValueType = inputValue;
-//     // alert(`Your input is "${inputValue}"`)
-//     return inputValue;
-// }
-// /** Get selected option */
-// function getOptions (){
-//     let selectedOption = document.getElementById("i-select").value;
-//     // alert(`You selected "${document.getElementById("i-select").value}"`)
-//     return selectedOption;
-// }
-//
-// /** Write output to screen*/
-// function writeOutput (input) {
-//     let output = document.getElementById("o-text");
-//     output.innerHTML = input;
-//     return output;
-// }
-//
-// /** Validate input*/
-// function validateInput (input){
-//     alert(`Eingabe: "${input}"`)
-//     alert(`Typ der Eingabe: "${typeof input}"`);
-// }
-//
-// function logOutput () {
-//     let logEntry = document.createElement('div');
-//     logEntry.className = "log-entry";
-//     logEntry.innerHTML = "test";
-//     /** Test write output */
-//     const time = new Date();
-//     const timeStr = time.toLocaleTimeString();
-//
-//     let outPutString =`${timeStr}--${getInput()}--${getOptions()}`;
-//     document.body.append(outPutString);
-// }
-//
-// function doValidation () {
-//     /** Test function of button */
-//     //alert("button clicked!");
-//
-//     /** Test get input value */
-//     //alert(`Return from input: ${testGetInput()}`);
-//     alert(`Type of input: ${typeof getInput()}`);
-//
-//     /** Test get selected option */
-//     //alert(`Return from options. ${testOptions()}`);
-//
-//     /** Test write output */
-//     const time = new Date();
-//     const timeStr = time.toLocaleTimeString();
-//
-//     let outPutString =`${timeStr}--${getInput()}--${getOptions()}`;
-//     writeOutput(outPutString);
-//
-//     /** validate input*/
-//     //validateInput(testGetInput());
-//
-//     /** Test logging output*/
-//     logOutput();
-// }
